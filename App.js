c4ec1ebfd815f20cf29f445014494d2a90ad6343
Maksym <maksum.dygas@gmail.com>
2023-03-18 04:53:33 +0200

    Created repository and first push project

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button, SafeAreaView, StyleSheet, View, StatusBar } from "react-native";
+
+
+function App() {
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
