1d0e2765b63a78a01b508c40c945ebaca9f41135
Maksym <maksum.dygas@gmail.com>
2023-03-18 19:10:49 +0200

    Підключив кастомні SVG, як реакт компонент. Перевірив лише на Андроїді. Змінив назву проекту на нижній регістр

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { Cars, TravelCar, VillageHouse } from "./assets/svgImages";
 
 
 function App() {
@@ -8,10 +9,19 @@ function App() {
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar backgroundColor="#284eb4" />
       <View style={styles.background}>
-        <Text style={styles.textBold}>I`m added custom fonts to my app. This is a Bold font</Text>
-        <Text style={styles.textSemiBold}>This is a SemiBold font</Text>
-        <Text style={styles.textMedium}>This is a Medium font</Text>
-        <Text style={styles.textRegular}>This is a Regular font</Text>
+        <View style={styles.forSvg}>
+          <View style={styles.forTwoSvgInRow}>
+            <Cars width={"45%"} height={150} fill={"#ded9d9"} />
+            <VillageHouse width={"45%"} height={150} fill={"#1a1c1a"} />
+          </View>
+          <TravelCar width={"90%"} height={100} style={styles.car} />
+        </View>
+        <View style={styles.forText}>
+          <Text style={styles.textBold}>I`m added custom fonts to my app. This is a Bold font</Text>
+          <Text style={styles.textSemiBold}>This is a SemiBold font</Text>
+          <Text style={styles.textMedium}>This is a Medium font</Text>
+          <Text style={styles.textRegular}>This is a Regular font</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -23,6 +33,23 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#284eb4",
     paddingHorizontal: 15,
+  },
+  forTwoSvgInRow: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  forSvg: {
+    marginTop: 30,
+  },
+  car: {
+    margin: 0,
+    padding: 0,
+    marginVertical: 30,
+    fill: "#1a1c1a",
+    marginLeft: "5%",
+  },
+  forText: {
+    flex: 1,
   },
   textBold: {
     fontFamily: "EduNSWACTFoundation-Bold",
---
+++ assets/svgImages/index.js
@@ -0,0 +1,7 @@
+import Cars from './Cars.svg'
+import TravelCar from './TravelCar.svg'
+import VillageHouse from './VillageHouse.svg'
+
+export {Cars}
+export {TravelCar}
+export {VillageHouse}
